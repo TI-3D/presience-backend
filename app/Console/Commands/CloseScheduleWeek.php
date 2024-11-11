07e775a5323326be0c55c91ac3fc2d085f26ba05
putricha_ <putrinorchasana@gmail.com>
2024-11-11 23:15:23 +0700

    feat : add scheduler for auto alpha if student not present

--- app/Console/Commands/CloseScheduleWeek.php
+++ app/Console/Commands/CloseScheduleWeek.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -27,29 +28,81 @@ class CloseScheduleWeek extends Command
      */
     public function handle()
     {
-        $today = Carbon::today()->format('Y-m-d');
-        $scheduleWeeks = DB::table('schedule_weeks as sw')
-            ->join('schedules as s', 'sw.schedule_id', '=', 's.id')
-            ->join('rooms as r', 's.room_id', '=', 'r.id')
-            ->join('lecturers as l', 's.lecturer_id', '=', 'l.id')
-            ->join('courses as c', 's.course_id', '=', 'c.id')
-            ->join('weeks as w', 'sw.week_id', '=', 'w.id')
-            ->where('sw.date', $today)
-            ->select('sw.*', 's.*', 'r.*', 'sw.id as sw_id', 'r.name as room_name', 'l.name as lecturer_name', 'c.*', 'c.name as course_name', 'w.*')
-            ->get();
+        try {
+            $today = Carbon::today()->format('Y-m-d');
+            $scheduleWeeks = DB::table('schedule_weeks as sw')
+                ->join('schedules as s', 'sw.schedule_id', '=', 's.id')
+                ->join('rooms as r', 's.room_id', '=', 'r.id')
+                ->join('lecturers as l', 's.lecturer_id', '=', 'l.id')
+                ->join('courses as c', 's.course_id', '=', 'c.id')
+                ->join('weeks as w', 'sw.week_id', '=', 'w.id')
+                ->where('sw.date', $today)
+                ->select('sw.*', 's.*', 'r.*', 'sw.id as sw_id', 'r.name as room_name', 'l.name as lecturer_name', 'c.*', 'c.name as course_name', 'w.*')
+                ->get();
+            $scheduleWeeks->each(function ($schedule) {
+                $students = DB::table('schedule_weeks as sw')
+                    ->join('schedules as s', 'sw.schedule_id', '=', 's.id')
+                    ->join('groups as g', 's.group_id', '=', 'g.id')
+                    ->join('users as u', 'g.id', '=', 'u.group_id')
+                    ->where('sw.id', $schedule->sw_id)
+                    ->select('*')
+                    ->get();
+                $endTime = Carbon::parse($schedule->end_time);
+                if (Carbon::now()->gte($endTime)) {
+                    if ($schedule->status == 'opened') {
+                        DB::table('schedule_weeks')
+                            ->where('id', $schedule->sw_id)
+                            ->update([
+                                'status' => 'closed',
+                                'closed_at' => now(),
+                            ]);
+                        $this->info("Schedule week ID {$schedule->sw_id} has been closed");
+                        $students->each(function ($student) use ($schedule) {
+                            $count = $this->searchAttendance($student, $schedule);
+                            if ($count == 0) {
+                                $this->createAlphaAttendance($schedule->time, $student->id, $schedule->sw_id);
+                            } else {
+                                return $this->info("Student id {$student->id} already present");
+                            }
+                        });
+                    } else {
+                        $this->info("Schedule week ID {$schedule->sw_id} has been closed by lecturer");
+                        $students->each(function ($student) use ($schedule) {
+                            $count = $this->searchAttendance($student, $schedule);
+                            if ($count == 0) {
+                                $this->createAlphaAttendance($schedule->time, $student->id, $schedule->sw_id);
+                            } else {
+                                return $this->info("Student id {$student->id} already present");
+                            }
+                        });
+                    }
+                }
+            });
+            $this->info("Closed schedule weeks where end time has passed");
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-        $scheduleWeeks->each(function ($schedule) {
-            if ($schedule->end_time >= now()) {
-                DB::table('schedule_weeks')
-                ->where('id', $schedule->sw_id)
-                    ->update([
-                        'status' => 'closed',
-                        'closed_at' => now(),
-                    ]);
-                $this->info("Schedule week ID {$schedule->sw_id} has been closed.");
-            }
-        });
+    public function searchAttendance($student, $schedule)
+    {
+        $count = DB::table('attendances')
+            ->where('student_id', $student->id)
+            ->where('schedule_week_id', $schedule->sw_id)
+            ->exists();
+        return $count;
+    }
 
-        $this->info("Closed schedule weeks where end time has passed.");
+    public function createAlphaAttendance($time, $id, $sw_id)
+    {
+        $attendanceId = DB::table('attendances')->insert([
+            'alpha' => $time,
+            'sakit' => 0,
+            'izin' => 0,
+            'student_id' => $id,
+            'schedule_week_id' => $sw_id,
+            'entry_time' => now(),
+        ]);
+        return $this->info("Attendances student id {$id} has been added.");
     }
 }
