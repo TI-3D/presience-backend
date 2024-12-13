060d2124fe247dd2298c7840146b1af548c83166
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-12-13 14:47:54 +0700

    feat: add well-known route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/.well-known/assetlinks.json', function () {
+    return response()->file(public_path('.well-known/assetlinks.json'));
+});
