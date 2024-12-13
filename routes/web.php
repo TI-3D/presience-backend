e2f8c1ee5b96d9e4e0f80bcc3691de7b06a58d48
Taufiq Hidayatulloh <hidayatullohtaufiq0@gmail.com>
2024-12-13 16:32:45 +0700

    update: revert Changes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/app/.well-known/assetlinks.json', function () {
+Route::get('/.well-known/assetlinks.json', function () {
     return response()->file(public_path('.well-known/assetlinks.json'));
 });
